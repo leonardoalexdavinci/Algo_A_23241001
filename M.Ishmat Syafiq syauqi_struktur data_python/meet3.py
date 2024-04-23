e749fc1c56255f3b813a2871e6ec828ad3933071
leonardoalexdavinci <145946853+leonardoalexdavinci@users.noreply.github.com>
2024-04-23 11:57:54 +0800

    meet3 tugas pyton M.Ishmat syafiq syauqi 23241001

---
+++ M.Ishmat Syafiq syauqi_struktur data_python/meet3.py
@@ -0,0 +1,24 @@
+bil_integer = 100
+print (type(bil_integer))
+
+print("tipe data dari bil_integer adalah",(type(bil_integer)))
+
+bil_float = 2.5
+print (type(bil_float))
+print("tipe data dari bil_integer adalah",(type(bil_float)))
+
+bil_String = "hakki"
+print (type(bil_String))
+print("tipe data dari bil_integer adalah",(type(bil_String)))
+
+
+bil_bol = False
+print (type(bil_bol))
+print("tipe data dari bil_integer adalah",(type(bil_bol)))
+
+#tipe data dari C
+
+from ctypes import c_double
+bil_doubel = c_double(3000000)
+print (type(bil_doubel))
+
